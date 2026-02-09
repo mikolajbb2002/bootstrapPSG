2150f447a76c025d1f3ff6dde4a670dfadcf1948
Miki <mikolajbb2002@users.noreply.github.com>
2026-02-09 15:01:01 +0100

    feat(pipeline): Added apply and destroy dispatch

--- bootstrap/03_main.tf
+++ bootstrap/03_main.tf
@@ -12,19 +12,19 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_container" "core" {
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id  = azurerm_storage_account.storage.id
   name                  = var.core_storage_container_name
   container_access_type = var.storage_container_access_type
 }
 
 resource "azurerm_storage_container" "platformlz" {
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id  = azurerm_storage_account.storage.id
   name                  = var.platformlz_storage_container_name
   container_access_type = var.storage_container_access_type
 }
 
 resource "azurerm_storage_container" "applz" {
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id  = azurerm_storage_account.storage.id
   name                  = var.applz_storage_container_name
   container_access_type = var.storage_container_access_type
 }
